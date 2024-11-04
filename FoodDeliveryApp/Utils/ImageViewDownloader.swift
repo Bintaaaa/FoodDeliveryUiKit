36f160b871cd06052ddcd8f2cbe9c29592dbb890
Bintaaaa <bijantium.yt.32@gmail.com>
2024-11-05 00:11:59 +0700

    compress image

--- FoodDeliveryApp/Utils/ImageViewDownloader.swift
+++ FoodDeliveryApp/Utils/ImageViewDownloader.swift
@@ -9,6 +9,8 @@ import UIKit
 
 
 extension UIImageView{
+    
+    
     /// <#Description#>
     /// - Parameter url: to be loaded
     func load(url: URL){
@@ -16,10 +18,19 @@ extension UIImageView{
             if let data = try? Data(contentsOf:url){
                 if let image = UIImage(data: data){
                     DispatchQueue.main.async{
-                        self.image = image
+                        self.image = image.resized(to: CGSize(width: 64, height: 64))
                     }
                 }
             }
+        }
+    }
+}
+
+extension UIImage {
+    func resized(to targetSize: CGSize) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+        return renderer.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: targetSize))
         }
     }
 }
