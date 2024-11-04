70e4ebdb5f6a2496918a6c0e00f8ddf3283800f2
Bintaaaa <bijantium.yt.32@gmail.com>
2024-11-04 14:18:11 +0700

    Initial Commit

---
+++ FoodDeliveryAppTests/FoodDeliveryAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryAppTests.swift
+//  FoodDeliveryAppTests
+//
+//  Created by Bijantyum on 04/11/24.
+//
+
+import Testing
+@testable import FoodDeliveryApp
+
+struct FoodDeliveryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
